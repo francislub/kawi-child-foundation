e2f9c8b6efcc568d14bfc3c4805e6240111dae42
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2025-01-04 13:22:00 -0800

    more

--- app/education/page.jsx
+++ app/education/page.jsx
@@ -17,6 +17,9 @@ import educationImage2 from '../public/images/programs/2.jpg';
 import educationImage3 from '../public/images/programs/3.jpg';
 import bannerImage from '../public/images/banner.jpg'
 
+import HeaderSection from "../components/header";
+import FooterSection from "../components/footer";
+
 export default function HomePage() {
     const [isOpen, setIsOpen] = useState(false);
 
@@ -27,71 +30,7 @@ export default function HomePage() {
     return (
         <div className="scroll-smooth text-gray-800">
             <div className="fullContainer bannerd" id="homeSection">
-                <header>
-                    <div className="container">
-                    <Link href="/">
-                        <Image 
-                        src={logoImage} 
-                        height={100} 
-                        width={100} // Add width to prevent layout shift
-                        alt="Foundation Logo" 
-                        className="rounded-md"
-                        />
-                    </Link>
-
-                        <nav className="pt-5 relative">
-                            <div className="flex items-center justify-between px-4 py-3 md:flex md:space-x-4">
-                                <div className="flex justify-between items-center w-full md:w-auto">
-                                {!isOpen && (
-                                    <button
-                                        className="md:hidden text-gray-900 focus:outline-none relative top-4 right-4 z-50"
-                                        onClick={() => setIsOpen(!isOpen)}
-                                    >
-                                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-                                        </svg>
-                                    </button>
-                                )}
-                                </div>
-                                <ul className={`mt-4 md:mt-0 md:flex md:space-x-4 ${isOpen ? 'block' : 'hidden'}`}>
-                                    <li className="dropdown relative">
-                                        <a href="#" className="dropbtn link ">ABOUT</a>
-                                        <div className="dropdown-content absolute hidden shadow-lg rounded-md">
-                                            <Link href="/about" className="link block px-4 py-2 text-gray-700">WHO WE ARE</Link>
-                                            <Link href="/wedo" className="link block px-4 py-2 text-gray-700">WHAT WE DO</Link>
-                                            <Link href="/impact" className="link block px-4 py-2 text-gray-700">OUR IMPACT</Link>
-                                            <Link href="/team" className="link block px-4 py-2 text-gray-700">OUR LEADERSHIP</Link>
-                                            <Link href="/joinus" className="link block px-4 py-2 text-gray-700">JOIN OUR TEAM</Link>
-                                            <Link href="/benef" className="link block px-4 py-2 text-gray-700">BENEFICIARIES</Link>
-                                        </div>
-                                    </li>
-                                    <li className="dropdown relative">
-                                        <a href="#" className="dropbtn link">PROGRAMS</a>
-                                        <div className="dropdown-content absolute hidden shadow-lg rounded-md">
-                                            <Link href="/education" className="link block px-4 py-2 text-gray-700 bg-[#1f8cad] rounded-md">EDUCATION</Link>
-                                            <Link href="/community" className="link block px-4 py-2 text-gray-700">COMMUNITY ENGAGEMENT</Link>
-                                        </div>
-                                    </li>
-                                    <li className="dropdown relative">
-                                        <a href="#" className="dropbtn link">CHAPTERS</a>
-                                        <div className="dropdown-content absolute hidden shadow-lg rounded-md">
-                                            <Link href="/denmark" className="link block px-4 py-2 text-gray-700">DENMARK</Link>
-                                            <Link href="/switzerland" className="link block px-4 py-2 text-gray-700">SWITZERLAND</Link>
-                                            <Link href="/germany" className="link block px-4 py-2 text-gray-700">GERMANY</Link>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <Link href="/gallery" className="link block px-4 py-2 text-gray-700">Gallery</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/paypal" className="link bg-amber-700 rounded-lg block px-4 py-2 text-white">Donate</Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </nav>
-
-                    </div>
-                </header>
+                <FooterSection />
                 {/* Header ends here */}
 
                 <div className="container space-y-10">
@@ -219,103 +158,7 @@ export default function HomePage() {
 
             {/* Join us section ends here */}
 
-            <footer className="">
-                <div className="container flex md:flex-row md:justify-between md:space-x-10 space-y-5 flex-col">
-
-                    <div className="newsLetterContainer md:w-96">
-                        <Image src={logoImage} 
-                        width={80}
-                        className="rounded-md pb-5" 
-                        alt="Logo Image"/>
-                        <p>
-                            For 15 years, we have committed to providing access to quality education for volunarable yet talented refugees. Donate today and help us create more refugee change makers like Ntakamaze Nziyonvira.
-                        </p>
-                        <input 
-                            type="text" 
-                            placeholder="Enter your email address" />
-                    </div>
-                    {/* NewsLetter container ends here */}
-
-                    <div className="linksContainer">
-                        <div className="title">
-                            Useful Links
-                        </div>
-                        <ul className="space-y-3">
-                            <li>
-                                <Link href="/" className="link">
-                                    Home
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/about" className="link">
-                                    About Us
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/programs" className="link">
-                                    Programs
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/education" className="link">
-                                    Education
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/gallery" className="link">
-                                    Gallery
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/joinus" className="link">
-                                    Join Us
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                    {/* LinksContainer ends here */}
-
-                    <div className="connectContainer space-y-6">
-                        <div className="title flex items-center">
-                            follow us 
-                            
-                        </div>
-                        <div className="flex items-center gap-4">
-                            <Image src={logoImage1} width={50} className="rounded-full pb-0 border-yellow-500 border-2 cursor-pointer hover:border-blue-500" alt="Logo Image"/>
-                            <Image src={logoImage2} width={50} className="rounded-full pb-0 border-yellow-500 border-2 cursor-pointer hover:border-green-500" alt="Logo Image"/>
-                            <Image src={logoImage3} width={50} className="rounded-full pb-0 border-yellow-500 border-2 cursor-pointer hover:border-red-500" alt="Logo Image"/>
-                            <Image src={logoImage4} width={50} className="rounded-full pb-0 border-yellow-500 border-2 cursor-pointer hover:border-purple-500" alt="Logo Image"/>
-                            <Image src={logoImage5} width={50} className="rounded-full pb-0 border-yellow-500 border-2 cursor-pointer hover:border-yellow-600" alt="Logo Image"/>
-                        </div>
-
-
-
-
-                        <span>Mailing address:</span>
-                        <br />
-                        <span>Phone:</span>
-                        <span>Email:</span> info@abcde.org
-                        <br />
-                        <p>
-                            Contact - Uganda <br />
-                        </p>
-                        <p>
-                            Contact - Denmark <br />
-                        </p>
-                        <p>
-                            Contact - Switzerland <br />
-                        </p>
-                        <span>Mailing address:</span>
-                        <br />
-                        <span>Physical address:</span>
-                        <br />
-                        <span>Phone:</span> +256-
-                        <span>Email:</span> info@abced.org
-                    </div>
-                    {/* ConnectContainer ends here */}
-
-                </div>
-            </footer>
+            <FooterSection />
 
 
 
--- app/switzerland/page.jsx
+++ app/switzerland/page.jsx
@@ -12,6 +12,9 @@ import logoImage4 from '../public/icons/twi.png'
 import logoImage5 from '../public/icons/you.png'
 import { useRouter, useSearchParams } from 'next/navigation';
 
+import HeaderSection from "../components/header";
+import FooterSection from "../components/footer";
+
 export default function HomePage() {
     const [isOpen, setIsOpen] = useState(false);
 
@@ -59,71 +62,7 @@ export default function HomePage() {
     return (
         <div className="scroll-smooth">
             <div className="fullContainer bannerd" id="homeSection">
-                <header>
-                    <div className="container">
-                    <Link href="/">
-                        <Image 
-                        src={logoImage} 
-                        height={100} 
-                        width={100} // Add width to prevent layout shift
-                        alt="Foundation Logo" 
-                        className="rounded-md"
-                        />
-                    </Link>
-
-                        <nav className="pt-5 relative">
-                            <div className="flex items-center justify-between px-4 py-3 md:flex md:space-x-4">
-                                <div className="flex justify-between items-center w-full md:w-auto">
-                                {!isOpen && (
-                                    <button
-                                        className="md:hidden text-gray-900 focus:outline-none relative top-4 right-4 z-50"
-                                        onClick={() => setIsOpen(!isOpen)}
-                                    >
-                                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-                                        </svg>
-                                    </button>
-                                )}
-                                </div>
-                                <ul className={`mt-4 md:mt-0 md:flex md:space-x-4 ${isOpen ? 'block' : 'hidden'}`}>
-                                    <li className="dropdown relative">
-                                        <a href="#" className="dropbtn link ">ABOUT</a>
-                                        <div className="dropdown-content absolute hidden shadow-lg rounded-md">
-                                            <Link href="/about" className="link block px-4 py-2 text-gray-700">WHO WE ARE</Link>
-                                            <Link href="/wedo" className="link block px-4 py-2 text-gray-700">WHAT WE DO</Link>
-                                            <Link href="/impact" className="link block px-4 py-2 text-gray-700">OUR IMPACT</Link>
-                                            <Link href="/team" className="link block px-4 py-2 text-gray-700">OUR LEADERSHIP</Link>
-                                            <Link href="/joinus" className="link block px-4 py-2 text-gray-700">JOIN OUR TEAM</Link>
-                                            <Link href="/benef" className="link block px-4 py-2 text-gray-700">BENEFICIARIES</Link>
-                                        </div>
-                                    </li>
-                                    <li className="dropdown relative">
-                                        <a href="#" className="dropbtn link">PROGRAMS</a>
-                                        <div className="dropdown-content absolute hidden shadow-lg rounded-md">
-                                            <Link href="/education" className="link block px-4 py-2 text-gray-700">EDUCATION</Link>
-                                            <Link href="/community" className="link block px-4 py-2 text-gray-700">COMMUNITY ENGAGEMENT</Link>
-                                        </div>
-                                    </li>
-                                    <li className="dropdown relative">
-                                        <a href="#" className="dropbtn link">CHAPTERS</a>
-                                        <div className="dropdown-content absolute hidden shadow-lg rounded-md">
-                                            <Link href="/denmark" className="link block px-4 py-2 text-gray-700">DENMARK</Link>
-                                            <Link href="/switzerland" className="link block px-4 py-2 text-gray-700 bg-[#1f8cad] rounded-md">SWITZERLAND</Link>
-                                            <Link href="/germany" className="link block px-4 py-2 text-gray-700">GERMANY</Link>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <Link href="/gallery" className="link block px-4 py-2 text-gray-700">Gallery</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/paypal" className="link bg-amber-700 rounded-lg block px-4 py-2 text-white">Donate</Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </nav>
-
-                    </div>
-                </header>
+                <HeaderSection />
                 {/* Header ends here */}
 
                 <div className="container">
